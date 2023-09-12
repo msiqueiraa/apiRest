6d11b5a619fd40ded1dd6f1f7fe9d096d89ac429
msiqueiraa <matheus.ramosalmeida@hotmail.com>
2023-09-12 10:22:14 -0300

    refactor warning

--- src/main/java/com/example/apiRest/controllers/ProductController.java
+++ src/main/java/com/example/apiRest/controllers/ProductController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
 
 @RestController
 public class ProductController {
--- src/main/java/com/example/apiRest/services/ProductService.java
+++ src/main/java/com/example/apiRest/services/ProductService.java
@@ -4,14 +4,12 @@ import com.example.apiRest.controllers.ProductController;
 import com.example.apiRest.dtos.ProductRecordDto;
 import com.example.apiRest.models.ProductModel;
 import com.example.apiRest.repositories.ProductRepository;
-import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+
 
 import java.util.List;
 import java.util.Optional;
