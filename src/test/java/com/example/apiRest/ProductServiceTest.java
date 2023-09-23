22a3b31702779b413b314d5560e560330a44f44c
msiqueiraa <matheus.ramosalmeida@hotmail.com>
2023-09-23 04:00:11 -0300

    service layer test implemented partional

--- src/test/java/com/example/apiRest/ProductServiceTest.java
+++ src/test/java/com/example/apiRest/ProductServiceTest.java
@@ -16,6 +16,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.BeanUtils;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
@@ -33,17 +36,53 @@ public class ProductServiceTest {
     void setup() {
         BigDecimal big1 = new BigDecimal("1.5");
         productRecordDto = new ProductRecordDto("Guaravita", big1);
+        productModel = new ProductModel();
 
     }
-
     @Test
-    void MustSaveProduct() {
+    void mustSaveProduct() {
         BigDecimal big1 = new BigDecimal("1.5");
         ProductModel newProduct = new ProductModel();
         newProduct.setName("Guaravita");
         newProduct.setValue(big1);
         productRepository.save(newProduct);
-        var response =  productService.saveProduct(productRecordDto);
+        var response = productService.saveProduct(productRecordDto);
         assertNotNull(response);
     }
+
+    @Test
+    void mustGetAllProducts() {
+        List<ProductModel> productList = new ArrayList<>();
+        productList.add(new ProductModel());
+        productList.add(new ProductModel());
+        var response = productService.getAllProducts();
+        assertNotNull(response);
+    }
+
+    @Test
+    void MustGetOneProduct() {
+        UUID id = UUID.randomUUID();
+        var response = productService.getOneProduct(id);
+        assertNotNull(response);
+    }
+
+    @Test
+    void mustUpdateProduct() {
+        UUID id = UUID.randomUUID();
+        BigDecimal big1 = new BigDecimal("1.5");
+        ProductModel newProduct = new ProductModel();
+        newProduct.setName("Guaravita");
+        newProduct.setValue(big1);
+        productRepository.save(newProduct);
+        var response = productService.updateProduct(id, productRecordDto);
+        assertNotNull(response);
+    }
+
+    @Test
+    void mustDeleteProduct() {
+        UUID id = UUID.randomUUID();
+        var response = productService.deleteProduct(id);
+        assertNotNull(response);
+    }
+
 }
